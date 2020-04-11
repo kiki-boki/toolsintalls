008cbc0e0074752cbb7e71ca1ddefa688cefbcca
K1-Ind <63471866+K1-Ind@users.noreply.github.com>
2020-04-11 07:52:09 +0000

    Installtools
    ToolsintallFor Linux
    Install sqlmap
    Install metasploit 
    Intall Nmap
    Install Wireshark 
    Install John The Riper 
    Install Setoolkit
    Install Wpscan
    Install KatanaFramework

---
+++ toolsinstall.sh
@@ -0,0 +1,265 @@
+#!/bin/bash
+#jangan dicopy ya sayang
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+echo "starting tools... /";
+sleep 0.1;
+clear
+echo 'Starting tools... -';
+sleep 0.1;
+clear
+echo 'sTarting tools... \';
+sleep 0.1;
+clear
+echo "stArting tools... /";
+sleep 0.1;
+clear
+echo 'staRting tools... -';
+sleep 0.1;
+clear
+echo 'starTing tools... \';
+sleep 0.1;
+clear
+echo "startIng tools... /";
+sleep 0.1;
+clear
+echo "startiNg tools... -";
+sleep 0.1;
+clear
+echo 'startinG tools... \';
+sleep 0.1;
+clear
+echo 'starting Tools... -';
+sleep 0.1;
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo $red "▄▄▄█████▓ ▒█████   ▒█████   ██▓      ██████  ██▓███   ██▓      ▄████ "
+echo $red "▓  ██▒ ▓▒▒██▒  ██▒▒██▒  ██▒▓██▒    ▒██    ▒ ▓██░  ██▒▓██▒     ██▒ ▀█▒"
+echo $red "▒ ▓██░ ▒░▒██░  ██▒▒██░  ██▒▒██░    ░ ▓██▄   ▓██░ ██▓▒▒██░    ▒██░▄▄▄░"
+echo $red "░ ▓██▓ ░ ▒██   ██░▒██   ██░▒██░      ▒   ██▒▒██▄█▓▒ ▒▒██░    ░▓█  ██▓"
+echo $white "  ▒██▒ ░ ░ ████▓▒░░ ████▓▒░░██████▒▒██████▒▒▒██▒ ░  ░░██████▒░▒▓███▀▒"
+echo $white "  ▒ ░░   ░ ▒░▒░▒░ ░ ▒░▒░▒░ ░ ▒░▓  ░▒ ▒▓▒ ▒ ░▒▓▒░ ░  ░░ ▒░▓  ░ ░▒   ▒ "
+echo $white "    ░      ░ ▒ copyriht@2019 ░ Palembang Cyber Team░ ░ ░ ▒  ░  ░   ░ "
+echo $white "  ░      ░ ░ ░ ▒  ░ ░ ░ ▒    ░ ░   ░  ░  ░  ░░         ░ ░   ░ ░   ░ "
+echo $white "             ░ ░      ░ ░      ░  ░      ░               ░  ░      ░ "
+echo $red "============================[Pal-CT]============================"
+echo $purple "Author : k1k1 W1j@y@"
+echo $purple "Team   : Palembang Cyber Team"
+echo $purple "Github : https://github.com/K1-ct19"
+echo $red "================================================================"
+echo $red "================================================================"
+echo $yellow "                              Menu Install                   "
+echo $red "================================================================"
+echo $green "1]Install Sqlmap"
+echo $green "2]Install Metasploit"
+echo $green "3]Install Wireshark"
+echo $green "4]Install Nmap"
+echo $green "5]Install Setoolkit"
+echo $green "6]Install Katana Franetwork"
+echo $green "7]Install Wpscan"
+echo $green "8]Install JohnTheRiper"
+echo $red "================================================================"
+read -p"?] Silahkan Masukan Pilihan Anda [1/2/3/4/5/6/7/8]=" pilih
+case $pilih in
+
+1)
+echo "[Installing Sqlmap.......]"
+apt install python3
+cd
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python3 sqlmap.py
+;;
+2)
+echo "[Installing Metasploit..........]"
+apt update && upgrade
+cd
+git clone https://github.com/rapid7/metasploit-framework
+cd metasploit-framework
+ls
+;;
+3)
+echo "[Installing Wireshark]"
+cd
+apt install wireshark
+;;
+4)
+echo "[Installing Nmap]"
+apt install nmap
+;;
+5)
+echo "[Installing Setoolkit]"
+apt install set
+;;
+6)
+echo "[Installing KatanaFranetwork]"
+cd
+git clone https://github.com/PowerScript/KatanaFramework
+cd KatanaFramework
+ls
+;;
+7)
+echo "[Installing Wpscan]"
+apt install wpscan
+;;
+8)
+echo "[Installing JohnTheRiper]"
+apt install john
+;;
+esac
